394fc37273b0eba23440e1854f2ca8de3bdf2248
Valentin Bagi <bagi.valentin@gmail.com>
2020-05-18 10:24:23 +0200

    add screen for the main content

---
+++ screens/MainScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+const MainScreen = () => {
+    return(
+        <View>
+
+        </View>
+    )
+}
+
+export default MainScreen;
