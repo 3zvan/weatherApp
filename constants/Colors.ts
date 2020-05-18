4e068e62be9ab2a8b5c2c35056ecb65fb4d6469e
Valentin Bagi <bagi.valentin@gmail.com>
2020-05-18 16:15:40 +0200

    add color constants for background gradients

---
+++ constants/Colors.ts
@@ -0,0 +1,10 @@
+export default {
+    rainLight: '#e6dada',
+    rainDark: '#45656d',
+    clearLight: '#a8c0ff',
+    clearDark: '#5946ab',
+    stormLight: '#734b6d',
+    stormDark: '#654283',
+    snowLight: '#ffffff',
+    snowDark: '#abbaab',
+}
