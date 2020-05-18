5fa73b408a37c5f18fd9918574a0bc90ef5e840a
Valentin Bagi <bagi.valentin@gmail.com>
2020-05-18 15:08:39 +0200

    add model for the API reply

---
+++ models/weatherData.model.ts
@@ -0,0 +1,27 @@
+export interface WeatherData {
+    weather: [{
+        main: string,
+        description: string,
+        icon: string
+    }],
+    main: {
+        temp: number,
+        temp_min: number,
+        temp_max: number,
+        humidity: number,
+        pressure: number
+    },
+    wind: {
+        speed: number,
+        deg: number
+    },
+    clouds: {
+        all: number
+    },
+    name: string,
+    sys: {
+        country: string,
+        sunrise: number,
+        sunset: number
+    }
+}
